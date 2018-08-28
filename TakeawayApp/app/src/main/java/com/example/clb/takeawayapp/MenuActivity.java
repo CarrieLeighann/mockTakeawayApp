276cd9971f730ff2c58a51b897c9c1a7a7766198
Carrie Boardman <carrieboardman92@hotmail.com>
2018-08-28 12:51:09 +0100

    Improved home page UI and menu contents

--- TakeawayApp/app/src/main/java/com/example/clb/takeawayapp/MenuActivity.java
+++ TakeawayApp/app/src/main/java/com/example/clb/takeawayapp/MenuActivity.java
@@ -2,11 +2,13 @@ package com.example.clb.takeawayapp;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,9 +28,21 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         displayMenuView();
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void displayMenuView(){
